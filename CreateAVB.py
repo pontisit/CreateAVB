bba7a1e72e184ee8715f16022c35aca8149aa350
pontisit <72914936+pontisit@users.noreply.github.com>
2023-10-09 15:53:57 +0200

    Add files via upload

--- CreateAVB.py
+++ CreateAVB.py
@@ -4,7 +4,8 @@ import pandas as pd
 import sys
 from datetime import datetime
 
-ABC = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_-': /"
+ABC = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_-': /&"
+# DELABC = ","
 HUN = "áéíóöőõúüűûÁÉÍÓÖŐÕÚÜŰÛ"
 ENG = "aeioooouuuuAEIOOOOUUUU"
 
@@ -15,10 +16,9 @@ def to_alpha_numeric(input_str):
         hun_idx = HUN.find(char)
         if hun_idx >= 0:
             output_str += ENG[hun_idx]
-        elif char not in ABC:
-            output_str += "_"
-        else:
+        elif char in ABC:
             output_str += char
+
     return output_str
 
 
@@ -58,7 +58,9 @@ else:
     now = datetime.now()
     dt_string = now.strftime("_%Y-%m-%d_%H-%M-%S")
 
-    result_file = sys.argv[1].replace(".xlsx", dt_string + ".avb")
+    # result_file = sys.argv[1].replace(".xlsx", dt_string + ".avb")
+    result_file = "_DATA.avb"
+
     with avb.open() as f:
 
         for index, row in df.iterrows():
@@ -71,6 +73,8 @@ else:
                 comp = f.create.Composition(mob_type="CompositionMob")
                 comp.name = to_alpha_numeric(row["Programme"]).upper()
 
+                print(comp.name)
+
                 new_mob_att = f.create.Attributes()
 
                 new_mob_att["ProvysTask"] = provystask_dict[row["Type"]]
@@ -81,9 +85,12 @@ else:
                 new_mob_att["ProvysSubs"] = str(row["Completed subs"]).replace(
                     "nan", ""
                 )
-                new_mob_att["TapeID"] = row["House ID"].replace(
-                    "_01", "_" + str(i + 1).rjust(2, "0")
-                )
+                new_mob_att["TapeID"] = row["House ID"].replace("_01", "_" + str(i + 1).rjust(2, "0")).replace("-01", "-" + str(i + 1).rjust(2, "0"))
+
+                if new_mob_att["ProvysTask"] == "MPG":
+                    new_mob_att["TapeID"] += "MPG"
+
+                print(new_mob_att["TapeID"])
 
                 comp.attributes["_USER"] = new_mob_att
 
